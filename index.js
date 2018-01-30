89fd1ddf4459cac098a511c532ebfa69a35d59fb
Mike Geldard <mike.geldard@degree53.com>
2018-01-30 13:18:32 +0000

    Adds UAC elevation prompt and improved error handling

--- index.js
+++ index.js
@@ -3,19 +3,43 @@ var fs = require('fs');
 var path = require('path');
 
 var isWindows = process.platform === 'win32';
+var elevatePath = path.join(__dirname, 'bin', 'elevate.cmd');
 
-module.exports = function (directory, callback) {
+function execSyncElevated(command) {
+	execSync('"' + elevatePath + '" ' + command);
+}
+
+module.exports = function takeown (filePath, callback) {
+
 	if (!isWindows) {
 		return;
 	}
 
-	execSync('takeown /f "' + directory + '"');
+	try {
 
-	if (fs.lstatSync(directory).isDirectory()) {
-		execSync('takeown /f "' + directory + '" /r /d y');
+		var stats = fs.lstatSync(filePath);
+
+		if (stats.isDirectory()) {
+			execSyncElevated('takeown /f "' + filePath + '" /r /d y');
+		} else {
+			execSyncElevated('takeown /f "' + filePath + '"');
+		}
+
+		console.log('takeown:', filePath, 'has been seized.');
+
+	} catch (error) {
+
+		if (error.code === 'ENOENT') {
+			console.log('takeown:', filePath, 'does not exist.');
+		}
+
+		// fs.lstatSync can fail with EPERM on Windows
+		if (error.code === 'EPERM') {
+			execSyncElevated('takeown /f "' + filePath + '"');
+			takeown(filePath, callback);
+			return;
+		}
 	}
-
-	console.log('takeown:', directory, 'has been seized.');
 
 	callback();
 }
