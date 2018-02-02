161176912cf2ad4c3eea052f675f95dd627ec7bc
Mike Geldard <mike.geldard@degree53.com>
2018-02-02 10:32:26 +0000

    Adds thread blocking to ensure synchronicity and improved error handling

--- index.js
+++ index.js
@@ -2,18 +2,47 @@ var execSync = require('child_process').execSync;
 var fs = require('fs');
 var path = require('path');
 
-var isWindows = process.platform === 'win32';
-var elevatePath = path.join(__dirname, 'bin', 'elevate.cmd');
+var IS_WINDOWS = process.platform === 'win32';
+var ELEVATE_PATH = path.join(__dirname, 'bin', 'elevate.cmd');
 
-function execSyncElevated(command) {
-	execSync('"' + elevatePath + '" ' + command);
+function execSyncElevated (command) {
+	execSync('"' + ELEVATE_PATH + '" ' + command);
 }
 
-module.exports = function takeown (filePath, callback) {
+// Intentionally locks up javascript thread for a fixed number of
+// recursive calls to give Windows time to get its arse in gear.
+function attemptCallback(filePath, callback, attempts) {
 
-	if (!isWindows) {
+	try {
+
+		callback();
 		return;
+
+	} catch (error) {
+
+		if (attempts === 0) {
+			console.error('takeown: callback failed - exceeded attempts');
+			throw error;
+		}
+
+		switch (error.code) {
+
+			case 'EPERM':
+			case 'EACCES':
+			case 'EBUSY':
+			case 'ENOTEMPTY':
+				return attemptCallback(filePath, callback, --attempts);
+
+			default:
+				throw error;
+		}
 	}
+}
+
+// Calls fire and forget Windows specific command to seize control
+// of a file or directory. Never seems to fail but we don't get any
+// confirmation of success.
+function takeOwnership(filePath) {
 
 	try {
 
@@ -25,21 +54,49 @@ module.exports = function takeown (filePath, callback) {
 			execSyncElevated('takeown /f "' + filePath + '"');
 		}
 
-		console.log('takeown:', filePath, 'has been seized.');
+		return;
 
 	} catch (error) {
 
 		if (error.code === 'ENOENT') {
-			console.log('takeown:', filePath, 'does not exist.');
-		}
-
-		// fs.lstatSync can fail with EPERM on Windows
-		if (error.code === 'EPERM') {
-			execSyncElevated('takeown /f "' + filePath + '"');
-			takeown(filePath, callback);
+			console.warn('takeown:', filePath, 'does not exist.');
 			return;
 		}
+
+		// fs.lstatSync can fail with EPERM on Windows so retry
+		// after taking control of this specific path.
+		if (error.code === 'EPERM') {
+			execSyncElevated('takeown /f "' + filePath + '"');
+			return takeOwnership(filePath);
+		}
+	}
+}
+
+module.exports = function takeown (filePath, callback, attempts) {
+
+	if (!IS_WINDOWS) {
+		callback();
+		return;
 	}
 
-	callback();
+	try {
+
+		callback();
+		return;
+
+	} catch (error) {
+
+		switch (error.code) {
+
+			case 'EPERM':
+			case 'EACCES':
+			case 'EBUSY':
+			case 'ENOTEMPTY':
+				takeOwnership(filePath);
+				return attemptCallback(filePath, callback, attempts || 3000);
+
+			default:
+				throw error;
+		}
+	}
 }
